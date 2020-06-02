383cf2a2578858c4770fe60ff30a89ace9b25076
dimitristrechas <dimitristrechas@gmail.com>
2020-06-02 23:49:51 +0300

    ant design loaded

---
+++ pages/blog/[slug].js
@@ -0,0 +1,44 @@
+import React from "react";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Head from "next/head";
+import marked from "marked";
+
+const Post = ({ htmlString, data }) => {
+  return (
+    <>
+      <Head>
+        <title>{data.title}</title>
+      </Head>
+      <div dangerouslySetInnerHTML={{ __html: htmlString }} />
+    </>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const files = fs.readdirSync("posts");
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const markdown = fs.readFileSync(path.join("posts", slug + ".md")).toString();
+  const parsedMarkdown = matter(markdown);
+  const htmlString = marked(parsedMarkdown.content);
+
+  return {
+    props: { htmlString, data: parsedMarkdown.data },
+  };
+};
+
+export default Post;
---
+++ posts/test.md
@@ -0,0 +1,8 @@
+---
+title: Test
+description: "A test blog post"
+---
+
+# Test@
+
+Hello World
