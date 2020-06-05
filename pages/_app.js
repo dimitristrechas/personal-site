c5c8144863ce4a2848898cc9ad843a0b5e1193b8
dimitristrechas <dtrechas@singularlogic.eu>
2020-06-05 16:48:37 +0300

    react-icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,52 @@
 import Link from "next/link";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "../styles/App.scss";
+import { FaDev } from "react-icons/fa";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Navbar bg="light" expand="lg">
-        <Link href="/" as={"/"}>
-          <Navbar.Brand href="/">Dimitris Trechas</Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Link href="/" as={"/"}>
-              <Nav.Link href="/">Home</Nav.Link>
-            </Link>
-            <Link href="/blog" as={"/blog"}>
-              <Nav.Link href="/blog">Blog</Nav.Link>
-            </Link>
-            <Nav.Link href="/">Bio</Nav.Link>
-            <Nav.Link href="/">Portofolio</Nav.Link>
-            <Nav.Link href="/">Contact</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-primary">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
       <Container>
-        <Component {...pageProps} />
+        <Navbar expand="lg">
+          <Link href="/" as={"/"}>
+            <Navbar.Brand href="/" className="d-inline-flex align-items-center">
+              <FaDev />
+              Dimitris Trechas
+            </Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link href="/" as={"/"}>
+                <Nav.Link href="/">Home</Nav.Link>
+              </Link>
+              <Link href="/blog" as={"/blog"}>
+                <Nav.Link href="/blog">Blog</Nav.Link>
+              </Link>
+              <Nav.Link href="/">Bio</Nav.Link>
+              <Nav.Link href="/">Portofolio</Nav.Link>
+              <Nav.Link href="/">Contact</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-primary">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <>
+          <Component {...pageProps} />
+        </>
       </Container>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { BsTrophy } from "react-icons/bs";
+import { IconContext } from "react-icons";
+
 const Home = ({ slugs }) => {
-  return "Home page";
+  return (
+    <Row>
+      <Col>
+        <IconContext.Provider value={{ size: "20px" }}>
+          <BsTrophy />
+        </IconContext.Provider>
+      </Col>
+    </Row>
+  );
 };
 
 export default Home;
