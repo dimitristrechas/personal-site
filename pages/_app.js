7ddcaafb4572bbfbd6581aa7370dcbc5e1e5a014
dimitristrechas <dimitristrechas@gmail.com>
2020-06-08 17:53:47 +0300

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import Container from "react-bootstrap/Container";
 import Header from "../components/Header/Header";
+import Head from "next/head";
 import "../styles/App.scss";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Dimitris Trechas - Personal Site</title>
+      </Head>
       <Container>
         <Header />
       </Container>
