ff635cf21f15b7e88f50bd34daf2ca1790512f01
Dimitris Trechas <dimitristrechas@gmail.com>
2021-01-23 13:52:28 +0200

    getFullYear() in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: FC = () => {
     <div className="navbar pt-5 pb-3">
       <div className="container justify-content-center">
         <div className="row">
-          <div className="col footer">{`Copyright © 2020 Dimitris Trechas`}</div>
+          <div className="col footer">{`Copyright © ${new Date().getFullYear()} Dimitris Trechas`}</div>
         </div>
       </div>
     </div>
