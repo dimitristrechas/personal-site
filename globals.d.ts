76d0964dd281d0e22c50a8f8ab468872af08b2f0
Dimitris Trechas <dimitristrechas@gmail.com>
2024-01-26 19:47:40 +0200

    types

---
+++ globals.d.ts
@@ -0,0 +1,45 @@
+type Post = {
+  id: string;
+  attributes: {
+    title: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    updatedAt: string;
+    createdAt: string;
+    tags: {
+      data: Tag[];
+    };
+    slug: string;
+  };
+};
+
+type Tag = {
+  id: string;
+  attributes: {
+    title: string;
+    color: string;
+  };
+};
+
+type PageData = { title: string };
+
+type ContactPage = {
+  data: {
+    id: string;
+    attributes: {
+      content: string;
+      title: string;
+    };
+  };
+};
+
+type AboutPage = {
+  data: {
+    id: string;
+    attributes: {
+      content: string;
+      title: string;
+    };
+  };
+};
