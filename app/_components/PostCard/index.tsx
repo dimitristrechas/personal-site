48f8564d6fab8f2ce3f89f4476f5a2cd0420b0ae
Dimitris Trechas <dimitristrechas@gmail.com>
2025-10-21 08:41:16 +0300

    feat: show created date and use Intl.DateTimeFormat

--- app/_components/PostCard/index.tsx
+++ app/_components/PostCard/index.tsx
@@ -10,9 +10,16 @@ const PostCard: FC<InputProps> = ({ post, isLastPost }: InputProps) => {
   return (
     <Link key={post.id} href={`/blog/${post.slug}`} prefetch={true}>
       <div className={`${isLastPost ? "" : "border-b-1"} py-4 cursor-pointer`}>
-        <div className="text-xl text-gray-800">{post.title}</div>
+        <div className="text-xl text-gray-800 mb-1">{post.title}</div>
         <div className="text-sm">
-          {new Date(post.updatedAt).toLocaleString("el-GR")}
+          {new Intl.DateTimeFormat(
+            Intl.DateTimeFormat().resolvedOptions().locale,
+            {
+              dateStyle: "medium",
+              timeStyle: "short",
+              timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+            },
+          ).format(new Date(post.createdAt))}
         </div>
         <div className="mt-2">
           {post.tags?.length > 0
